446a4c185cff410dd1e01c543ffe2b32703db45f
Marynel Jaspe <mjaspe94@gmail.com>
2024-07-12 23:32:38 -0300

    se agrega permiso jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -8,7 +8,7 @@ const createToken = (user, res) => {
   };
 
   const token = jwt.sign(usuarioParaToken, SECRET_KEY_JWT, { expiresIn: '1h' });
-  res.cookie('pupysSS', token, { httpOnly: false, secure: false, maxAge: 3600000, path: '/' });
+  res.cookie('pupysSS', token, { httpOnly: false, secure: false, maxAge: 3600000, path: '/',sameSite:'None' });
 };
 
 module.exports = createToken;
