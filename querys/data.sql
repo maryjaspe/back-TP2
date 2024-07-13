cf9a96eb6318721a9f3ff40731986f047bc99d30
Marynel Jaspe <mjaspe94@gmail.com>
2024-07-12 23:56:08 -0300

    se agrega variable de entorno se modifica los productos

--- querys/data.sql
+++ querys/data.sql
@@ -20,28 +20,28 @@ INSERT INTO
   )
 VALUES
   (
-    'Producto 1',
+    'Torta de Zanahoria',
     'Descripción del producto 1',
     100.00,
     10,
     'Categoría 1',
-    'https://images.unsplash.com/photo-1598759310798-18f8497d9858?fm=jpg&w=3000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D'
+    'https://i.pinimg.com/736x/f1/09/d0/f109d009cd7608e903ff8728f0e00bfb.jpg'
   ),
   (
-    'Producto 2',
+    'Galleta',
     'Descripción del producto 2',
     150.00,
     20,
     'Categoría 2',
-    'https://static.nationalgeographicla.com/files/styles/image_3200/public/nationalgeographic_1468962.jpg?w=1600&h=900'
+    'https://arquivet.com/2408-thickbox_default/galletas-sandwich-hueso-2-kg.jpg'
   ),
   (
-    'Producto 3',
+    'Donas',
     'Descripción del producto 3',
     200.00,
     30,
     'Categoría 3',
-    'https://img.freepik.com/vector-gratis/personaje-dibujos-animados-gatito-ojos-dulces_1308-135596.jpg'
+    'https://i.pinimg.com/236x/7a/c9/d6/7ac9d6e08953ed71d9c1ffc25b1b3cd4.jpg'
   );
 
 INSERT INTO
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const SECRET_KEY_JWT = process.env.SECRET_KEY_JWT;
+const IS_JWT_SECURE = process.env.IS_JWT_SECURE === 'true';
+
 const createToken = (user, res) => {
   const usuarioParaToken = {
     id: user.id,
@@ -7,7 +9,7 @@ const createToken = (user, res) => {
   };
 
   const token = jwt.sign(usuarioParaToken, SECRET_KEY_JWT, { expiresIn: '1h' });
-  res.cookie('pupysSS', token, { httpOnly: false, secure: true, maxAge: 3600000, path: '/',sameSite:'None' });
+  res.cookie('pupysSS', token, { httpOnly: false, secure: IS_JWT_SECURE, maxAge: 3600000, path: '/',sameSite:'None' });
 };
 
 module.exports = createToken;
